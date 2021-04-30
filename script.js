469246c30dba88f23798b25810fc9f5730533ebe
[strodivaryann] <[sem.mm@mail.ru]>
2021-05-01 00:34:04 +1000

    ok

---
+++ script.js
@@ -0,0 +1,31 @@
+/*  Night mode */
+
+const content = document.body;
+const main = document.querySelectorAll('description', 'p', 'a', 'h2');
+const nightMode = document.getElementById("dark-change");
+const menu = document.getElementById('menu')
+const bar = document.getElementsByClassName('navigation')
+const lWidth = window.screen.width
+
+nightMode.addEventListener('click', function () {
+  nightMode.classList.toggle('active');
+  content.classList.toggle('night');
+})
+
+function events() {
+  for (var i = 0; i < bar.length; i++) {
+    bar[i].style.display = 'block';
+  }
+}
+function block() {
+  for (var i = 0; i < bar.length; i++) {
+    bar[i].style.display = 'none';
+  }
+}
+
+menu.addEventListener(('mouseover'), events);
+menu.addEventListener(('click'), block);
+
+
+
+
